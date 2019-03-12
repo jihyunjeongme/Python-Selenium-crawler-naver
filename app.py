564eabb355ee3cfa6dfa8d8d57441176fdd41aab
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-03-13 00:52:32 +0900

    code updating

--- app.py
+++ app.py
@@ -19,16 +19,15 @@ from bs4 import BeautifulSoup
 # find_element_by_class_name('some_class_name')
 # find_element_by_tag_name('h1')
 
-## Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
-driver = webdriver.Chrome(
-    "/Users/jihyun/Documents/GitHub/Python-Selenium-crawler-naver/chromedriver"
-)
 ## PhantomJS의 경우 | 아까 받은 PhantomJS의 위치를 지정해준다.
 # driver = webdriver.PhantomJS(
 #     "/Users/jihyun/Documents/GitHub/Python-Selenium-crawler-naver/phantomjs-2.1.1-macosx/bin/phantomjs"
 # )
 
+
 options = webdriver.ChromeOptions()
+
+options.add_argument("headless")
 options.add_argument("window-size=1920x1080")
 options.add_argument("disable-gpu")
 options.add_argument(
@@ -36,31 +35,40 @@ options.add_argument(
 )
 options.add_argument("lang=ko_KR")
 
-time.sleep(3.5)
+## Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
+driver = webdriver.Chrome(
+    "/Users/jihyun/Documents/GitHub/Python-Selenium-crawler-naver/chromedriver",
+    chrome_options=options,
+)
+
+time.sleep(5 + random.random() * 0.3)
+
 ## 암묵적으로 웹 자원 로드를 위해 3초까지 기다려 준다.
-driver.implicitly_wait(3)
+# driver.implicitly_wait(3)
 
 ## url에 접근한다.
 driver.get("https://nid.naver.com/nidlogin.login")
 
-time.sleep(3.5)
+time.sleep(5 + random.random() * 0.3)
 
 ## 아이디/비밀번호를 입력해준다.
-driver.find_element_by_name("id").send_keys("stothyuni***")
-time.sleep(2 + random.random() * 0.3)
+driver.find_element_by_name("id").send_keys("stothyu&&**ni")
+time.sleep(5 + random.random() * 0.3)
 
-driver.find_element_by_name("pw").send_keys("gosd**kdi1***")
-time.sleep(4)
+driver.find_element_by_name("pw").send_keys("gos^dk**^^di1")
+time.sleep(5 + random.random() * 0.3)
 
 ## 로그인 버튼을 눌러주자.
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
-time.sleep(2)
+time.sleep(15)
 
-# Naver 페이 들어가기
-driver.get("https://order.pay.naver.com/home")
+# Naver 메일 들어가기
+driver.get("https://mail.naver.com/?n=1552402737365&v=f")
 html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
-notices = soup.select("div.p_inr > div.p_info > a > span")
+
+# Naver 메일 뽑아 내기
+notices = soup.findAll("strong", {"class": "mail_title"})
 
 for n in notices:
     print(n.text.strip())
